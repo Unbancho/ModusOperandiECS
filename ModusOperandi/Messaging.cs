1c921aa1df78c6411b9b132acfcf00c5d6fc6557
Unbancho <badsilverwolf@gmail.com>
2020-03-10 22:02:46 +0000

    Add project files.

---
+++ ModusOperandi/Messaging.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+
+namespace ModusOperandi.Messaging
+{
+    [Flags]
+    public enum MessageFlags
+    {
+        None = 0
+    }
+
+    [Serializable]
+    public class Message : ISerializable
+    {
+        public override string ToString() { return Text; }
+        public Dictionary<string, object> PackedObjects;
+
+        public object this[string name]
+        {
+            get { return PackedObjects[name]; }
+            set { PackObject(value, name); }
+        }
+
+        public object Sender;
+
+        public MessageFlags Flag;
+
+        private MessageFlags[] _flagsArray;
+        private MessageFlags[] FlagsArray
+        {
+            get { return _flagsArray; }
+            set { _flagsArray = value; foreach (var f in value) { Flag |= f; } }
+        }
+
+        public string Text;
+
+        public Message(string text = null, params Enum[] flags)
+        {
+            PackedObjects = new Dictionary<string, object>();
+            Text = text == null ? String.Empty : text;
+            FlagsArray = Array.ConvertAll(flags, f => (MessageFlags)f);
+        }
+
+        public void PackObject<T>(T obj, string name = null)
+        {
+            if (name == null)
+            {
+                name = obj.GetType().Name;
+            }
+            PackedObjects[name] = obj;
+        }
+
+        public object UnpackObject(string name, bool pop = false)
+        {
+            object obj = PackedObjects[name];
+            if (pop) PackedObjects.Remove(name);
+            return obj;
+        }
+
+        public object Pop(string name = null)
+        {
+            if (name == null)
+            {
+                foreach (KeyValuePair<string, object> entry in PackedObjects)
+                {
+                    return (obj: UnpackObject(entry.Key, pop: true), name: entry.Key);
+                }
+                throw new EmptyMessageException();
+            }
+            return UnpackObject(name, pop: true);
+        }
+
+        public bool TryPop(out object result, string name = null)
+        {
+            result = null;
+            try
+            {
+                result = Pop(name);
+                return true;
+            }
+            catch (Exception e) when (e is EmptyMessageException || e is KeyNotFoundException)
+            {
+                return false;
+            }
+        }
+
+        public IEnumerable<Enum> GetFlags(bool enumerable = true)
+        {
+            if (!enumerable) yield return Flag;
+            foreach (var flag in FlagsArray)
+            {
+                yield return flag;
+            }
+        }
+
+        public void SetFlags<T>(T flags)
+        {
+            List<MessageFlags> newFlags = new List<MessageFlags>();
+            foreach (var f in Enum.GetValues(flags.GetType()))
+            {
+                if ((flags as Enum).HasFlag((Enum)f))
+                    newFlags.Add((MessageFlags)f);
+            }
+            FlagsArray = newFlags.ToArray();
+        }
+
+        public void SetFlags(Enum[] flags)
+        {
+            FlagsArray = Array.ConvertAll(flags, item => (MessageFlags)item); ;
+        }
+
+        public bool HasFlag(Enum flag)
+        {
+            return Flag.HasFlag((MessageFlags)flag);
+        }
+
+        protected Message(SerializationInfo info, StreamingContext context)
+        {
+            PackedObjects = new Dictionary<string, object>();
+            foreach (SerializationEntry entry in info)
+            {
+                PackObject(entry.Value, entry.Name);
+            }
+            Text = (string)Pop("Text");
+            FlagsArray = (MessageFlags[])Pop("FlagsArray");
+            Flag = (MessageFlags)Pop("Flag");
+        }
+
+        public virtual void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            foreach (KeyValuePair<string, object> entry in PackedObjects)
+            {
+                info.AddValue(entry.Key, entry.Value);
+            }
+            info.AddValue("Text", Text);
+            info.AddValue("FlagsArray", FlagsArray);
+            info.AddValue("Flag", Flag);
+        }
+    }
+
+    [Serializable]
+    public class Package
+    {
+        public string Name { get; set; }
+        public object Object { get; set; }
+        public PropertyInfo PropertyInfo { get; set; }
+
+        public Package(object obj, PropertyInfo propertyInfo, string name)
+        {
+            Object = obj;
+            Name = name;
+        }
+    }
+
+    public class MessageProcessor
+    {
+        public static MessageProcessor operator +(MessageProcessor p1, MessageProcessor p2)
+        {
+            return new MessageProcessor()
+            {
+                ProcessingMethods = (p1.ProcessingMethods
+                .Concat(p2.ProcessingMethods
+                .Where(x => !p1.ProcessingMethods.ContainsKey(x.Key))))
+                .ToDictionary(x => x.Key, x => x.Value)
+            };
+        }
+
+        public Dictionary<Enum, MethodInfo> ProcessingMethods { get; set; }
+
+        private Type _flagType;
+        public Type FlagType { get { return _flagType; } set { if (value.IsDefined(typeof(FlagsAttribute), false)) _flagType = value; else throw new InvalidFlagType(value); } }
+
+        public virtual void ProcessMessage(Message message)
+        {
+            object[] parametersToPass = new object[] { message };
+            MethodInfo method;
+            foreach (Enum flag in message.GetFlags())
+            {
+                var e = (Enum)Convert.ChangeType((Enum)Enum.ToObject(FlagType, flag), FlagType);
+                if (ProcessingMethods.TryGetValue(e, out method))
+                    method.Invoke(this, parametersToPass);
+            }
+        }
+    }
+
+    public class EmptyMessageException : Exception { }
+    public class InvalidFlagType : Exception
+    {
+        public InvalidFlagType(Type type) : base($"Invalid type for flag: {type}. Must be Enum Flag.") { }
+    }
+
+}
---
+++ ModusOperandi/Networking.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ModusOperandi.Networking
+{
+    public abstract class ConnectionTcp
+    {
+        public readonly int PORT_NO;
+        public readonly string SERVER_IP;
+
+        public delegate void ConnectionHandler(object obj, ConnectionArgs args);
+        public delegate void MessageReceivedHandler(object obj, MessageReceivedArgs args);
+
+        public bool IsLocalConnection;
+
+        public CancellationTokenSource CancellationSource;
+
+        public void Stop()
+        {
+            CancellationSource.Cancel();
+        }
+
+        protected ConnectionTcp(string ip, int port)
+        {
+            PORT_NO = port;
+            SERVER_IP = ip;
+        }
+    }
+
+    public class ServerConnection : ConnectionTcp
+    {
+        public static implicit operator TcpListener(ServerConnection s) => s.Listener;  // No reason for this, I just thought it was epic
+        TcpListener Listener;
+        public bool Listening;
+
+        public event ConnectionHandler OnConnection;
+        public event MessageReceivedHandler OnMessageReceived;
+
+        public List<ClientConnection> Clients = new List<ClientConnection>();
+
+        public ServerConnection(int port) : base(NetworkUtilities.GetLocalIPAddress(), port)
+        {
+            IsLocalConnection = true;
+            Listener = new TcpListener(IPAddress.Parse(SERVER_IP), PORT_NO);
+        }
+
+        public void Run()
+        {
+            Listener.Start();
+            ListenForConnections();
+        }
+
+        private void StopListening()
+        {
+            Listener.Stop();
+            Listening = false;
+        }
+
+        public void ShutDown()
+        {
+            foreach (var client in Clients.ToArray())
+            {
+                Kick(client);
+            }
+            StopListening();
+        }
+
+        public async void ListenForConnections()
+        {
+            TcpClient newClient;
+            CancellationSource = new CancellationTokenSource();
+            Listening = true;
+            Console.WriteLine("Listening...");
+            using (CancellationSource.Token.Register(StopListening))
+            {
+                while (Listening)
+                {
+                    try
+                    {
+                        newClient = await AcceptClient(CancellationSource.Token);
+                        var processTask = Task.Run(() => ProcessClient(newClient));
+                    }
+                    catch (Exception e) when (e is NullReferenceException || e is ObjectDisposedException)
+                    {
+                        continue;
+                    }
+                }
+            }
+        }
+
+        private void ListenToClient(ClientConnection client)
+        {
+            Clients.Add(client);
+            Task.Run(() => ListenForMessages(client));
+        }
+
+        public async Task<TcpClient> AcceptClient(CancellationToken token)
+        {
+            try
+            {
+                TcpClient client = await Listener.AcceptTcpClientAsync();
+                return client;
+            }
+            catch (ObjectDisposedException e)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    return null;
+                }
+                throw e;
+            }
+        }
+
+        private void ProcessClient(TcpClient client)
+        {
+            ClientConnection clientConnection = new ClientConnection(client, SERVER_IP, PORT_NO);
+            Console.WriteLine(String.Format("New connection: {0}", client.Client.RemoteEndPoint));
+            ConnectionArgs args = new ConnectionArgs(clientConnection);
+            OnConnection?.Invoke(this, args);
+            ListenToClient(clientConnection);
+        }
+
+        public void ListenForMessages(ClientConnection client)
+        {
+            string clientIP = client.IPAddress;
+            object message = null;
+            while (true)
+            {
+                try
+                {
+                    message = ReceiveMessage(client);
+                    OnMessageReceived(this, new MessageReceivedArgs(message, client));
+                }
+                catch (Exception e) when (e is System.IO.IOException || e is InvalidOperationException)
+                {
+                    message = clientIP + " has disconnected.";
+                    Clients.Remove(client);
+                    if (!(e is InvalidOperationException))
+                    {
+                        ((TcpClient)client).Close();
+                    }
+                    break;
+                }
+            }
+        }
+
+        public object ReceiveMessage(ClientConnection client)
+        {
+            object dataReceived = NetworkUtilities.Receive(client);
+            if (dataReceived == null)
+            {
+                throw new System.InvalidOperationException();
+            }
+            return dataReceived;
+        }
+
+        public void Broadcast(object message)
+        {
+            foreach (ClientConnection client in this.Clients.ToArray())
+            {
+                SendMessageTo(message, client);
+            }
+        }
+
+        public void SendMessageTo(object message, ClientConnection client)
+        {
+            NetworkUtilities.Send(message, client);
+        }
+
+        public void Kick(ClientConnection client)
+        {
+            if (client != null)
+            {
+                Clients.Remove(client);
+                client.Stop();
+            }
+        }
+    }
+
+    public class ClientConnection : ConnectionTcp
+    {
+        public static implicit operator TcpClient(ClientConnection c) => c.Client; // No reason for this, I just thought it was epic
+        public event MessageReceivedHandler OnMessageReceived;
+        private TcpClient Client;
+        public bool Connected;
+
+        public new bool IsLocalConnection()
+        {
+            return IPAddress == NetworkUtilities.GetLocalIPAddress();
+        }
+
+        public string IPAddress
+        {
+            get
+            {
+                return Client.Client.RemoteEndPoint.ToString().Split(':')[0];
+            }
+        }
+
+        public int Port
+        {
+            get
+            {
+                return Int32.Parse(Client.Client.RemoteEndPoint.ToString().Split(':')[1]);
+            }
+        }
+
+        public ClientConnection(string ip, int port) : base(ip, port)
+        {
+            Client = new TcpClient(SERVER_IP, PORT_NO);
+            Connected = true;
+            Task.Run(() => ListenForMessages());
+        }
+
+        public ClientConnection(TcpClient client, string serverIP, int port) : base(serverIP, port)
+        {
+            Client = client;
+        }
+
+        private void Close()
+        {
+            Client.Close();
+        }
+
+        public void Send(object message)
+        {
+            NetworkUtilities.Send(message, Client);
+        }
+
+        public void ListenForMessages()
+        {
+            CancellationSource = new CancellationTokenSource();
+            using (CancellationSource.Token.Register(Close))
+            {
+                object message;
+                while (Connected)
+                {
+                    message = ReceiveMessage();
+                    Console.WriteLine(message);
+                    OnMessageReceived(this, new MessageReceivedArgs(message, null));
+                }
+            }
+        }
+
+        public object ReceiveMessage()
+        {
+            object message = NetworkUtilities.Receive(Client);
+            if (message == null)
+            {
+                Stop();
+            }
+            return message;
+        }
+
+        public new void Stop()
+        {
+            Connected = false;
+            Client.Close();
+        }
+    }
+
+
+    public static class NetworkUtilities
+    {
+        public static string GetLocalIPAddress()
+        {
+            string strHostName = Dns.GetHostName();
+            IPHostEntry ipHostEntry = Dns.GetHostEntry(strHostName);
+            IPAddress[] address = ipHostEntry.AddressList;
+            return address[address.Length - 1].ToString();
+        }
+
+        public static void Send(object message, TcpClient client)
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            binaryFormatter.Serialize(client.GetStream(), message);
+        }
+
+        public static object Receive(TcpClient client, CancellationToken token = new CancellationToken())
+        {
+            try
+            {
+                object dataReceived = DeserializeStream(client.GetStream());
+                return dataReceived;
+
+            }
+            catch (Exception e)
+            {
+                if (token.IsCancellationRequested || e is System.IO.IOException || e is SocketException || e is System.InvalidOperationException || e is System.Runtime.Serialization.SerializationException)
+                {
+                    return null;
+                }
+                throw e;
+            }
+        }
+
+        private static object DeserializeStream(NetworkStream networkStream)
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            return binaryFormatter.Deserialize(networkStream);
+        }
+    }
+
+    public class ConnectionArgs : EventArgs
+    {
+        private ClientConnection client;
+
+        public ConnectionArgs(ClientConnection newClient)
+        {
+            client = newClient;
+        }
+
+        public ClientConnection Client { get { return client; } } 
+    }
+
+    public class MessageReceivedArgs : EventArgs
+    {
+        private object message;
+        private ConnectionTcp sender;
+
+        public MessageReceivedArgs(object message, ConnectionTcp sender)
+        {
+            this.message = message;
+            this.sender = sender;
+        }
+
+        public object Message { get{ return message; } }
+
+        public ConnectionTcp Sender { get{ return sender; } }
+    }
+}
